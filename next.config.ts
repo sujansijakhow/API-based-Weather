73a9536db8f9feb68beb3dd2828b310904d0f821
sujansijakhow <sujansijakhow@gmail.com>
2025-03-14 20:00:49 +0545

    Configuration made

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    domains: [
+      'openweathermap.org'
+    ]
+  }
+}
+
 export default nextConfig;
