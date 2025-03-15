c412c0682b53336abffd764aa3e2641fcbfc610a
sujansijakhow <sujansijakhow@gmail.com>
2025-03-15 11:07:51 +0545

    Configuration updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",  // Allow `any` type
+      "react/react-in-jsx-scope": "off",           // No need for `import React`
+      "no-console": "warn",                        // Allow console.log but show a warning
+      "react-hooks/exhaustive-deps": "warn",       // Prevent missing dependencies in `useEffect`
+    },
+  },
 ];
 
 export default eslintConfig;
