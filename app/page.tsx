3225182699bdaac1afb4ab62f375eba6b566869a
sujansijakhow <sujansijakhow@gmail.com>
2025-03-15 10:57:42 +0545

    Error solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,81 @@
 "use client";
 
 import Head from "next/head";
-import axios from "axios"
-import { useState } from "react"
-import { BsSearch } from "react-icons/bs"
-
+import axios from "axios";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
 import Image from "next/image";
 import Weather from "@/components/Weather";
 import Spinner from "@/components/Spinner";
 
+interface WeatherData {
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+  }[];
+  name: string;
+}
+
 export default function Home() {
-  
   const [city, setCity] = useState('');
-  const [weather, setWeather] = useState<any>(null);
+  const [weather, setWeather] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
-  const fetchWeather = (e: React.FormEvent) => {
-    e.preventDefault()
+  const fetchWeather = async (e: React.FormEvent) => {
+    e.preventDefault();
     setLoading(true);
-    axios.get(url).then((response) => {
+
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+      const response = await axios.get(url);
       setWeather(response.data);
-      // console.log(response.data);
-    } )
-    setCity('');
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+
     setLoading(false);
+    setCity('');
+  };
+
+  if (loading) {
+    return <Spinner />;
   }
 
-  if(loading) {
-    return <Spinner />
-  } else {
+  return (
+    <div>
+      <Head>
+        <title>Weather System</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    return(
-      <div>
-        <Head>
-          <title>Weather System</title>
-          <link rel='icon' href='/favicon.ico' />
-    
-        </Head>
-    
-       {/* Overlay */}
-       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
-       {/* Background */}
-        <Image src="/background.avif" alt="Background Image" 
-        layout="fill"
-        // className="object-cover"
-        />
-    
-        {/* Search */}
-        <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10">
-          <form onSubmit={fetchWeather} className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-black-300 text-white rounded-2xl">
-            <div>
-              <input 
-              onChange={(e) => setCity(e.target.value)}
-              type="text" 
-              placeholder="Search City" 
-              className="bg-transparent border-none text-white focus:outline-none text-2xl" 
-              />
-            </div>
-            <button className="" onClick={fetchWeather}>
-              <BsSearch size={20} />
-            </button>
-          </form>
-        </div>
-    
-        {/* Weather */}
-    
-        {weather?.main && <Weather data={weather} />}
-    
-    
+      {/* Overlay */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
+
+      {/* Background */}
+      <Image src="/background.avif" alt="Background Image" layout="fill" />
+
+      {/* Search */}
+      <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10">
+        <form
+          onSubmit={fetchWeather}
+          className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-black-300 text-white rounded-2xl"
+        >
+          <input
+            onChange={(e) => setCity(e.target.value)}
+            type="text"
+            placeholder="Search City"
+            className="bg-transparent border-none text-white focus:outline-none text-2xl"
+          />
+          <button type="submit">
+            <BsSearch size={20} />
+          </button>
+        </form>
       </div>
-    )
-  }
 
-
+      {/* Weather Display */}
+      {weather && <Weather data={weather} />}
+    </div>
+  );
 }
--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import Image from "next/image";
 import spinner from '../public/spinner.gif';
 
-type Props = {
-};
-
-const Spinner: React.FC<Props> = ({  }) => {
-  return <div>
-    <Image className="w-[200px] m-auto block" src={spinner} alt="Spinner Image"/>
-  </div>;
+const Spinner: React.FC = () => { 
+  return (
+    <div>
+      <Image className="w-[200px] m-auto block" src={spinner} alt="Spinner Image" />
+    </div>
+  );
 };
 
 export default Spinner;
