d228515e43faadf06bda7387c3f7aa3b7b122503
sujansijakhow <sujansijakhow@gmail.com>
2025-03-15 10:47:08 +0545

    Feature added

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -12,16 +12,43 @@ const Weather: React.FC<Props> = ({ data }) => {
   console.log("Icon Code:", data?.weather?.[0]?.icon);
 
   return (
-    <div>
-      {data?.weather?.[0]?.icon && (
-        <Image 
-          src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
-          alt="Weather Icon"
-          width={100}
-          height={100}
-          unoptimized={true} 
-        />
-      )}
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+      {/* Top */}
+      <div className="relative flex justify-between pt-12">
+        <div className="flex flex-col gap-0 items-center">
+
+        {data?.weather?.[0]?.icon && (
+          <Image 
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
+            alt="Weather Icon"
+            width={80}
+            height={80}
+            priority={true} 
+          />
+        )}
+        <p className="text-xl ">{data.weather[0].main}</p>
+        </div>
+        <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;</p>
+      </div>
+      {/* Bottom */}
+
+      <div className="bg-black/50 relative p-8 rounded-md ">
+        <p className="text-2xl text-center pb-6">Weather in {data.name}</p>
+        <div className="flex justify-between text-center">
+          <div>
+            <p className="font-bold text-2xl">{data.main.feels_like.toFixed(0)}&#176;</p>
+            <p className="text-xl">Feels Like</p>
+          </div>
+          <div>
+            <p className="font-bold text-2xl">{data.main.humidity}%</p>
+            <p className="text-xl">Humidity</p>
+          </div>
+          <div>
+            <p className="font-bold text-2xl">{data.wind.speed.toFixed(0)} MPH</p>
+            <p className="text-xl">Winds</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
