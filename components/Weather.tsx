e7e6276c8e53e4ae7e99d21f19cc46b7e342b20f
sujansijakhow <sujansijakhow@gmail.com>
2025-03-15 11:39:01 +0545

    Displaying gap updated

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -30,7 +30,7 @@ const Weather: React.FC<Props> = ({ data }) => {
   // console.log("Icon Code:", data.weather?.[0]?.icon);
 
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+    <div className="relative flex flex-col justify-evenly gap-25 max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
       {/* Top Section */}
       <div className="relative flex justify-between pt-12">
         <div className="flex flex-col items-center">
